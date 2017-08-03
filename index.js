972579a1e4833aa62f8e560034d75bf6132168a4
Bitterulf <s_schlender@hotmail.com>
2017-08-03 07:11:49 +0200

    Add map loading

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const domready = require('domready');
 const cheerio = require('cheerio');
 const html2json = require('html2json').html2json;
+const json2html = require('html2json').json2html;
 
 xtag.register('s-svg', {
     lifecycle: {
@@ -94,6 +95,7 @@ xtag.register('st-travel', {
             const travelNode = html2json(this.outerHTML).child[0];
             const startCity = travelNode.attr.start;
 
+            const backgrounds = [];
             const cities = [];
             const roads = [];
 
@@ -116,10 +118,23 @@ xtag.register('st-travel', {
                 });
             });
 
+            travelNode.child.filter(function(child) {
+                return child.node == 'element' && child.tag == 'st-background';
+            }).forEach(function(backgroundNode) {
+                backgrounds.push({
+                    image: backgroundNode.attr.image,
+                    width: backgroundNode.attr.width,
+                    height: backgroundNode.attr.height,
+                    x: backgroundNode.attr.x,
+                    y: backgroundNode.attr.y
+                });
+            });
+
             this.state = {
                 idle: true,
                 currentCity: startCity,
                 currentAnimation: '',
+                backgrounds: backgrounds,
                 cities: cities,
                 roads: roads
             };
@@ -127,6 +142,8 @@ xtag.register('st-travel', {
         },
         inserted: function(){
             const state = this.state;
+
+            console.log('BG', this.state.backgrounds);
 
             const Component = {
                 view: function() {
@@ -178,12 +195,74 @@ xtag.register('st-travel', {
                             [
                                 m('style', roadPaths.join(' ')),
                                 m('h1', 'traveller'),
+                                m('div', {
+                                    style: {
+                                        position: 'absolute',
+                                        top: '0px',
+                                        left: '0px',
+                                        width: xWidth + 'px',
+                                        height: yWidth + 'px',
+                                        border: '1px solid blue',
+                                        overflow: 'hidden'
+                                    }
+                                },
+                                [
+                                    m('div', state.backgrounds.map(function(background) {
+                                        return m('div', {
+                                            style: {
+                                                backgroundImage: 'url("map.png")',
+                                                position: 'absolute',
+                                                left: parseInt(background.x) - xOffset + 'px',
+                                                top: parseInt(background.y) - yOffset + 'px',
+                                                width: background.width + 'px',
+                                                height: background.height + 'px'
+                                            }
+                                        });
+                                    })),
+                                    m('div', state.cities.map(function(city) {
+                                        if (state.currentCity == city.name) {
+                                            return m('div', {
+                                                style: {
+                                                    position: 'absolute',
+                                                    left: (city.x - xOffset) + 'px',
+                                                    top: (city.y - yOffset) + 'px',
+                                                    // width: 10 + 'px',
+                                                    // height: 10 + 'px',
+                                                    width: cityRadius * 2 + 'px',
+                                                    height: cityRadius * 2 + 'px',
+                                                    border: '1px solid green',
+                                                    'background-color': 'red',
+                                                    // 'border-radius': cityRadius + 'px',
+                                                    'text-align': 'center'
+                                                }
+                                            }, [
+                                                m('div', city.name)
+                                            ])
+                                        }
+
+                                        return m('div', {
+                                            style: {
+                                                position: 'absolute',
+                                                left: (city.x - xOffset) + 'px',
+                                                top: (city.y - yOffset) + 'px',
+                                                // width: 10 + 'px',
+                                                // height: 10 + 'px',
+                                                width: cityRadius * 2 + 'px',
+                                                height: cityRadius * 2 + 'px',
+                                                border: '1px solid grey',
+                                                'background-color': 'red',
+                                                // 'border-radius': cityRadius + 'px',
+                                                'text-align': 'center'
+                                            }
+                                        }, city.name)
+                                    }))
+                                ]),
                                 m('div',
                                     {
                                         style: {
                                             position: 'absolute',
-                                            top: '0px',
-                                            left: '0px',
+                                            left: 0 - xOffset + 'px',
+                                            top: 0 - yOffset + 'px',
                                             width: xWidth + 'px',
                                             height: yWidth + 'px',
                                             border: '4px solid yellow'
@@ -201,74 +280,34 @@ xtag.register('st-travel', {
                                             m('div', {
                                                 style: {
                                                     position: 'relative',
-                                                    top: cityRadius*-1+'px',
-                                                    left: cityRadius*-1+'px',
+                                                    top: 0*-1+'px',
+                                                    left: 0*-1+'px',
                                                     width: cityRadius * 2 + 'px',
                                                     height: cityRadius * 2 + 'px',
                                                     border: '1px solid magenta',
                                                     'text-align': 'center'
                                                 }
-                                            }, 'player')
+                                            }, [
+                                                m('div', state.roads.filter(function(road) { return road.from == state.currentCity }).map(function(road) {
+                                                    if (!state.idle) {
+                                                        return '';
+                                                    }
+
+                                                    return m('button', {
+                                                        onclick: function() {
+                                                            state.idle = false;
+                                                            state.currentAnimation = road.from+'_'+road.to+'  1s forwards'
+                                                            state.currentCity = road.to;
+                                                            window.setTimeout(function() {
+                                                                state.idle = true;
+                                                                m.redraw();
+                                                            }, 1000);
+                                                        }
+                                                    }, road.to);
+                                                }))
+                                            ])
                                         ])
                                     ]
-                                ),
-                                m('div', {
-                                    style: {
-                                        position: 'absolute',
-                                        top: '0px',
-                                        left: '0px',
-                                        width: xWidth + 'px',
-                                        height: yWidth + 'px',
-                                        border: '1px solid blue'
-                                    }
-                                }, state.cities.map(function(city) {
-                                    if (state.currentCity == city.name) {
-                                        return m('div', {
-                                            style: {
-                                                position: 'absolute',
-                                                left: (city.x - xOffset) + 'px',
-                                                top: (city.y - yOffset) + 'px',
-                                                width: cityRadius * 2 + 'px',
-                                                height: cityRadius * 2 + 'px',
-                                                border: '1px solid green',
-                                                'border-radius': cityRadius + 'px',
-                                                'text-align': 'center'
-                                            }
-                                        }, [
-                                            m('div', city.name),
-                                            m('div', state.roads.filter(function(road) { return road.from == state.currentCity }).map(function(road) {
-                                                if (!state.idle) {
-                                                    return '';
-                                                }
-
-                                                return m('button', {
-                                                    onclick: function() {
-                                                        state.idle = false;
-                                                        state.currentAnimation = road.from+'_'+road.to+'  1s forwards'
-                                                        state.currentCity = road.to;
-                                                        window.setTimeout(function() {
-                                                            state.idle = true;
-                                                            m.redraw();
-                                                        }, 1000);
-                                                    }
-                                                }, road.to);
-                                            }))
-                                        ])
-                                    }
-
-                                    return m('div', {
-                                        style: {
-                                            position: 'absolute',
-                                            left: (city.x - xOffset) + 'px',
-                                            top: (city.y - yOffset) + 'px',
-                                            width: cityRadius * 2 + 'px',
-                                            height: cityRadius * 2 + 'px',
-                                            border: '1px solid grey',
-                                            'border-radius': cityRadius + 'px',
-                                            'text-align': 'center'
-                                        }
-                                    }, city.name)
-                                })
                                 )
                             ]
                     )
@@ -281,5 +320,93 @@ xtag.register('st-travel', {
 });
 
 domready(function () {
+    fetch('map.json').then(function(response) {
+        return response.json();
+    }).then(function(map) {
 
+        const cities = [];
+        map
+            .layers
+            .filter(function(layer) { return layer.type == 'objectgroup'})
+            .forEach(function(layer) {
+                layer.objects.forEach(function(obj) {
+                    if (obj.ellipse) {
+                        console.log(obj);
+                        cities.push({
+                            name: obj.name,
+                            x: obj.x,
+                            y: obj.y
+                        });
+                    }
+                    else if(obj.polyline) {
+
+                    }
+                });
+            });
+
+        const backgrounds = [];
+        map
+            .layers
+            .filter(function(layer) { return layer.type == 'imagelayer'})
+            .forEach(function(layer) {
+                backgrounds.push({
+                    image: layer.image,
+                    width: layer.width * map.tilewidth,
+                    height: layer.height * map.tileheight,
+                    x: layer.x,
+                    y: layer.y
+                });
+            });
+
+        let childElements = [];
+
+        childElements = childElements.concat(
+            backgrounds.map(function(background) {
+                return {
+                    "node":"element",
+                    "tag":"game-background",
+                    "attr":{
+                        "image": background.image,
+                        "width": background.width,
+                        "height": background.height,
+                        "x": background.x,
+                        "y": background.y
+                    }
+                };
+            })
+        );
+
+        childElements = childElements.concat(
+            cities.map(function(city) {
+                return {
+                    "node":"element",
+                    "tag":"game-city",
+                    "attr":{
+                        "name": city.name,
+                        "x": city.x,
+                        "y": city.y
+                    }
+                };
+            })
+        );
+
+        const exampleStructure = {
+            "node":"root",
+            "child":[
+                {
+                    "node":"element",
+                    "tag":"game-map",
+                    "attr":{
+                        "width": map.width * map.tilewidth / 2,
+                        "height": map.height * map.tileheight / 2
+                    },
+                    "child": childElements
+                }
+            ]
+        };
+
+        const testResult = json2html(exampleStructure);
+
+        document.querySelector('#container').innerHTML = testResult;
+    });
 })
